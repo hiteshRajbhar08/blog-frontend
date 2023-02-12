19b7457c7fee3a9aa8ad17036514b662503fd410
Hitesh <h08rajbhar@gmail.com>
2023-02-12 15:09:30 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
+import AdminDashboardPage from './pages/admin/AdminDashboardPage';
+import CreatePostPage from './pages/createPost/CreatePostPage';
+import LoginPage from './pages/forms/LoginPage';
+import RegisterPage from './pages/forms/RegisterPage';
+import HomePage from './pages/home/HomePage';
+import PostPage from './pages/post/PostPage';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/posts" element={<PostPage />} />
+        <Route path="/posts/create-post" element={<CreatePostPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+      </Routes>
+    </Router>
   );
 };
 
