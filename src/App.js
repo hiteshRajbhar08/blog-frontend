1da82a8823625b5218d8e4212c752c5326b1829d
Hitesh <h08rajbhar@gmail.com>
2023-02-12 22:29:51 +0530

    category page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import AdminDashboardPage from './pages/admin/AdminDashboardPage';
+import CategoryPage from './pages/category/CategoryPage';
 import CreatePostPage from './pages/createPost/CreatePostPage';
 import LoginPage from './pages/forms/LoginPage';
 import RegisterPage from './pages/forms/RegisterPage';
@@ -18,9 +19,14 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/posts" element={<PostPage />} />
-        <Route path="/posts/create-post" element={<CreatePostPage />} />
-        <Route path="/posts/details/:id" element={<PostDetailsPage />} />
+
+        <Route path="posts">
+          <Route index element={<PostPage />} />
+          <Route path="create-post" element={<CreatePostPage />} />
+          <Route path="details/:id" element={<PostDetailsPage />} />
+          <Route path="categories/:category" element={<CategoryPage />} />
+        </Route>
+
         <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
       </Routes>
       <Footer />
---
+++ src/pages/category/CategoryPage.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import PostList from '../../components/posts/PostList';
+import { posts } from '../../dummyData';
+import './category.css';
+
+const CategoryPage = () => {
+  const { category } = useParams();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="category">
+      <h1 className="category-title">Posts based on {category}</h1>
+      <PostList posts={posts} />
+    </div>
+  );
+};
+
+export default CategoryPage;
