ab72ea0caa1d60c96f59bf459deff3b73ab56bca
Hitesh <h08rajbhar@gmail.com>
2023-02-13 13:02:23 +0530

    admin dashboard page

---
+++ src/pages/admin/AdminAddCategoryForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const AdminAddCategoryForm = () => {
+  const [title, setTitle] = useState('');
+
+  // From Submit Handler
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (title.trim() === '') return toast.error('Category Title is required');
+
+    console.log({ title });
+  };
+
+  return (
+    <div className="add-category">
+      <h6 className="add-category-title">Add New Category</h6>
+      <form onSubmit={formSubmitHandler} className="add-category-form">
+        <div className="add-category-form-group">
+          <label htmlFor="title">Category Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            id="title"
+            placeholder="Enter Category Title"
+          />
+        </div>
+        <button type="submit" className="add-category-btn">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AdminAddCategoryForm;
--- src/pages/admin/AdminDashboardPage.jsx
+++ src/pages/admin/AdminDashboardPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import AdminMain from './AdminMain';
+import AdminSidebar from './AdminSidebar';
+import './admin.css';
 
 const AdminDashboardPage = () => {
-  return <div>AdminDashboardPage</div>;
+  return (
+    <section className="admin-dashboard">
+      <AdminSidebar />
+      <AdminMain />
+    </section>
+  );
 };
 
 export default AdminDashboardPage;
